e8069432619ee9e276eed7c4e7eaa4f8b9ee1841
igorskyflyer <igor.dvlpr@gmail.com>
2021-07-18 01:42:14 +0200

    v.1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,59 @@
-# npm-valid-path
+## Valid Path
+
 🧰 Provides ways of testing whether a given value can be a valid file/directory name. 🏜
+
+<br>
+
+### Usage
+
+Install it by running,
+
+```shell
+npm i "@igor.dvlpr/valid-path"
+```
+
+<br>
+
+### API
+
+```ts
+function isValidPath(path: string, isFile = true): boolean
+```
+
+Returns whether the given path can be a valid file/directory name on the host machine.
+
+<br>
+
+```ts
+function isValidPathUnix(path: string, isFile = true): boolean
+```
+
+Returns whether the given path can be a valid file/directory name on Unix and Unix-like OS'.
+
+<br>
+
+```ts
+function isValidPathWin(path: string, isFile = true): boolean
+```
+
+Returns whether the given path can be a valid file/directory name on Windows OS.
+
+<br>
+
+### Examples
+
+```js
+const { isValidPathUnix, isValidPathWin, isValidPath } = require('@igor.dvlpr/valid-path')
+
+console.log(isValidPathUnix('hello&world.js', true)) // prints true
+console.log(isValidPathUnix('hello/world.js', true)) // prints false
+console.log(isValidPathUnix('hello/world')) // prints true
+
+console.log(isValidPathWin('hello/world.js', true)) // prints false
+console.log(isValidPathWin('hello/world', false)) // prints false
+console.log(isValidPathWin('CON7')) // prints false
+console.log(isValidPathWin('lpt1')) // prints false
+console.log(isValidPathWin('hello&world.js', true)) // prints true
+
+// isValidPath() internally uses the appropriate method for supported OS' (Unix-like and Windows)
+```
---
+++ main.js
@@ -0,0 +1,83 @@
+const { platform } = require('os')
+const { stringsInString, Position, charsInString } = require('@igor.dvlpr/chars-in-string')
+const { slash } = require('@igor.dvlpr/upath')
+
+const isWindows = platform() === 'win32'
+const winMaxPath = 260
+// Windows doesn't allow these characters to appear in the path
+const winNotAllowed = ['/', ':', '*', '?', '"', '<', '>', '|', "'"]
+// prettier-ignore
+const winDevices = [ 'CON',  'PRN',  'AUX',  'NUL',  'COM1',  'COM2',  'COM3',  'COM4',  'COM5',  'COM6',  'COM7',  'COM8',  'COM9',  'LPT1',  'LPT2',  'LPT3',  'LPT4',  'LPT5',  'LPT6',  'LPT7',  'LPT8',  'LPT9' ]
+
+const unixMaxPath = 255
+// Unix systems don't allow \0 (and a forward slash if it's a file name) as a part of the path
+const unixNotAllowed = []
+
+/**
+ * @private
+ * @param {string} path
+ * @param {string[]} notAllowedChars
+ * @param {number} maxPath
+ * @param {boolean} [isFile=true]
+ * @param {string} [separator=slash]
+ * @returns {boolean}
+ */
+function validPath(path, notAllowedChars, maxPath, isFile = true, separator = slash) {
+  if (!path || typeof path !== 'string') {
+    return false
+  }
+
+  const count = path.length
+
+  if (count === 0 || count > maxPath) {
+    return false
+  }
+
+  const notAllowed = notAllowedChars.slice()
+
+  if (isFile) {
+    notAllowed.push(separator)
+  }
+
+  return !charsInString(notAllowed, path)
+}
+
+/**
+ * Returns whether the given path can be a valid file/directory name on Windows OS.
+ * @param {string} path
+ * @param {boolean} [isFile=true]
+ * @returns {boolean}
+ */
+function isValidPathWin(path, isFile = true) {
+  return !stringsInString(winDevices, path, Position.Start, false) && validPath(path, winNotAllowed, winMaxPath, isFile, '\\')
+}
+
+/**
+ * Returns whether the given path can be a valid file/directory name on Unix and Unix-like OS'.
+ * @param {string} path
+ * @param {boolean} [isFile=true]
+ * @returns {boolean}
+ */
+function isValidPathUnix(path, isFile = true) {
+  return validPath(path, unixNotAllowed, unixMaxPath, isFile, '/')
+}
+
+/**
+ * Returns whether the given path can be a valid file/directory name on the host machine.
+ * @param {string} path
+ * @param {boolean} [isFile=true]
+ * @returns {boolean}
+ */
+function isValidPath(path, isFile = true) {
+  if (isWindows) {
+    return isValidPathWin(path, isFile)
+  } else {
+    return isValidPathUnix(path, isFile)
+  }
+}
+
+module.exports = {
+  isValidPath,
+  isValidPathUnix,
+  isValidPathWin,
+}

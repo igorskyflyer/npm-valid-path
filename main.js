b13761147ac994efdea3eed77003448295e6fbb8
igorskyflyer <igor.dvlpr@gmail.com>
2021-07-18 02:15:53 +0200

    Remove single quote as not allowed char on Windows (fixes #2)

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const { slash } = require('@igor.dvlpr/upath')
 const isWindows = platform() === 'win32'
 const winMaxPath = 260
 // Windows doesn't allow these characters to appear in the path
-const winNotAllowed = ['/', ':', '*', '?', '"', '<', '>', '|', "'"]
+const winNotAllowed = ['/', ':', '*', '?', '"', '<', '>', '|']
 // prettier-ignore
 const winDevices = [ 'CON',  'PRN',  'AUX',  'NUL',  'COM1',  'COM2',  'COM3',  'COM4',  'COM5',  'COM6',  'COM7',  'COM8',  'COM9',  'LPT1',  'LPT2',  'LPT3',  'LPT4',  'LPT5',  'LPT6',  'LPT7',  'LPT8',  'LPT9' ]
 
